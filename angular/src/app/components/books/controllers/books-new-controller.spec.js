f6db829ccc734d74c56429be239ed5586486e13a
Ziga <ziga.spindler@gmail.com>
2015-05-31 21:57:29 +0200

    Fixed Step 4

--- angular/src/app/components/books/controllers/books-new-controller.spec.js
+++ angular/src/app/components/books/controllers/books-new-controller.spec.js
@@ -1,24 +1,22 @@
 'use strict';
 
 describe('BooksNewController', function() {
-  var ctrl;
+  var ctrl, mockBackend;
 
   beforeEach(function() {
     module('mylibrary.books.controllers.new');
     module('mylibrary.books.services');
     module('ngResource');
-    inject(function($controller, $q, Book) {
+    inject(function($controller, $httpBackend) {
       ctrl = $controller('BooksNewController');
-
-      var deferred = $q.defer();
-      deferred.resolve({
+      mockBackend = $httpBackend;
+      mockBackend.expectPOST('/api/books').respond({
         id: 1,
         title: 't1',
         author: 'a1',
         read: true,
         pages: 100
       });
-      spyOn(Book, 'createNew').and.returnValue(deferred.promise);
     });
   });
 
@@ -32,14 +30,13 @@ describe('BooksNewController', function() {
     };
 
     ctrl.save();
+    mockBackend.flush();
     expect(ctrl.books.length).toBe(1);
-    expect(ctrl.books).toEqual([{
-      id: 1,
-      title: 't1',
-      author: 'a1',
-      read: true,
-      pages: 100
-    }]);
+    expect(ctrl.books[0].id).toEqual(1);
+    expect(ctrl.books[0].title).toEqual('t1');
+    expect(ctrl.books[0].author).toEqual('a1');
+    expect(ctrl.books[0].read).toEqual(true);
+    expect(ctrl.books[0].pages).toEqual(100);
     expect(ctrl.book).toEqual({});
   });
 });
