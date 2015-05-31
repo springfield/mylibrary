ab6c4921a3d26f74861be83cd91c04afd7fc4c97
Ziga <ziga.spindler@gmail.com>
2015-05-31 20:23:54 +0200

    Step 3

--- angular/src/app/components/books/controllers/books-list-controller.js
+++ angular/src/app/components/books/controllers/books-list-controller.js
@@ -2,6 +2,8 @@
 
 angular.module('mylibrary.books.controllers.list', [])
   .controller('BooksListController', function() {
+    this.orderBy = 'title';
+
     this.bookReadClass = function(book) {
       return book.read ? 'glyphicon-ok text-success' : 'glyphicon-remove text-danger';
     };
